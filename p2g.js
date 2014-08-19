071aabe364a52aaebf2ad3ea29a508eada13ea29
Tim Sogard <src@timsogard.com>
2014-08-19 00:23:38 -0700

    Initial commit, working for basic + array msgs

---
+++ p2g.js
@@ -0,0 +1,126 @@
+// Doc note on default metric resolution + report windows (and xFileFactor aggregation warning)
+// x- expand pin.report into metrics per pin
+//   ?- ignore disabled pins (mode -1)
+// accept prefix (<pre[.]fix>.<troop_name>.<scout_name>)
+// handle troop/scout add messages in stream (add to troops map)
+// offline/online events entered with value of one, to be used with drawAsInfinite
+//   other events to catch?
+var pinoccio = require('pinoccio'),
+	graphite = require('graphite'),
+	async = require('async'),
+	sprintf = require('util').format;
+
+var p_api = pinoccio('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+var graphite_client = graphite.createClient('plaintext://graphite.example.com:2003')
+var troops = {}
+
+function sanitize_name(name) {
+	return name.replace(/[^ A-Za-z0-9_-]/g, '').replace(/\s+/g, '_')
+}
+
+function get_troops(callback) {
+	//console.log("Troops")
+	p_api.rest({url:'/v1/troops'}, function(err, troop_data) {
+		for (var i=0; i < troop_data.length; i++) {
+			troop_id = troop_data[i]['id'];
+			troop_name = sanitize_name(troop_data[i]['name'])
+			troops[troop_id] = {name:troop_name}
+			console.log(sprintf("Learned troop named %s with id %d", troop_name, troop_id));
+		}
+		callback()
+	})
+}
+
+function get_scouts(callback) {
+	//console.log("Scouts")
+	troop_keys = Object.keys(troops)
+	//console.log("Got so many " + troop_keys.length)
+	async.each(troop_keys, function(troop_id, callback){
+		p_api.rest({url:'/v1/'+troop_id+'/scouts'}, function(err, scout_data) {
+			for (var j=0; j < scout_data.length; j++) {
+				scout_id = scout_data[j]['id']
+				scout_name = sanitize_name(scout_data[j]['name'])
+				console.log(sprintf("Learned scout named %s in troop %s with id %d", scout_name, troop_name, scout_id));
+				troops[troop_id][scout_id] = scout_name
+			}
+			callback()
+		})},
+		//function() {console.log(troops)}
+		function() {}
+		);
+
+	callback()
+}
+
+function get_events() {
+	syncer = p_api.sync()
+	syncer.on('data', function(data) {
+		handle_event(data)
+	});
+	syncer.on('error',function(err){
+		console.log('sync error: ',err)
+		delete syncer
+		//setTimeout(get_events, 60000)
+		setTimeout(get_events, 6000)
+	});
+	syncer.on('end',function(){
+		console.log("shouldn't end but depending on arguments it may");
+		delete syncer
+		//setTimeout(get_events, 60000)
+		setTimeout(get_events, 6000)
+	});
+}
+
+
+function handle_event(msg) {
+	if (!('troop' in msg['data']) || !('value' in msg['data'])) {
+		return
+	}
+	msg_data = msg['data']
+	msg_time = msg_data['time']
+	msg_value = msg_data['value']
+
+	//console.log("Got data", msg_data)
+
+	troop_name = troops[msg_data['troop']]['name']
+	scout_name = troops[msg_data['troop']][msg_data['scout']]
+	g_msg_prefix = ['pinoccio', troop_name, scout_name, msg_data['type']].join('.')
+
+	function add_metric(name, value) {
+		metric_key = [g_msg_prefix, name].join('.')
+		graphite_msg[metric_key] = value
+	}
+
+	graphite_msg = {}
+	for (var prop in msg_value) {
+		// skip metadata
+		if (prop == "_t" || prop == "type") {
+			continue
+		}
+		// if not a number
+		if (!isFinite(msg_value[prop])) {
+			// but an array, expand keys
+			if (msg_value[prop] instanceof Array ) {
+				for (var ar_i = 0; ar_i < msg_value[prop].length; ar_i++) {
+					add_metric([prop, ar_i].join('_'), msg_value[prop][ar_i])
+				}
+			} else {
+				continue
+			}
+		} else { // 1-dimensional value
+			add_metric(prop, msg_data['value'][prop])
+		}
+	}
+	if (Object.keys(graphite_msg).length == 0) {
+		console.log("Empty metric msg")
+		return
+	}
+	console.log("Metric", graphite_msg)
+	graphite_client.write(graphite_msg, msg_time, function(err) {
+		if (err !== null) {
+			console.log("Error from graphite:", err)
+		}
+	})
+}
+
+async.series([get_troops, get_scouts], get_events)
